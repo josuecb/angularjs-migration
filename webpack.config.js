56ddaccab5a39471e27bea280a016c91cb1df308
Josue C <anonjacmdev@hotmail.com>
2023-01-31 16:54:10 -0500

    6. Dual Boot

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./src/app/main.ts",
     output: {
@@ -15,5 +17,14 @@ module.exports = {
         rules: [
             { test: /\.tsx?$/, loader: "ts-loader" }
         ]
-    }
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            // if you have anymore problems tweet me at @gdi2290
+            // The (\\|\/) piece accounts for path separators for Windows and MacOS
+            /(.+)?angular(\\|\/)core(.+)?/,
+            path.join(__dirname, 'src'), // location of your src
+            {} // a map of your routes 
+        )
+    ]    
 }
